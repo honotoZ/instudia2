686e6d67a3a0b6af7a22b9c15e526111659116a9
honoto <honotozhimomi@gmail.com>
2024-05-14 20:17:41 +0530

    my-commit

--- Desktop/capstone-vsc/backend-food-app/backend-micro-service-food-app/src/main/java/com/entity/Login.java
+++ Desktop/capstone-vsc/backend-food-app/backend-micro-service-food-app/src/main/java/com/entity/Login.java
@@ -1,4 +1,3 @@
-
 package com.entity;
 
 import jakarta.persistence.Entity;
--- Desktop/capstone-vsc/backend-food-app/backend-micro-service-food-app/src/test/java/com/example/backendmicroservicefoodapp/BackendMicroServiceFoodAppApplicationTests.java
+++ Desktop/capstone-vsc/backend-food-app/backend-micro-service-food-app/src/test/java/com/example/backendmicroservicefoodapp/BackendMicroServiceFoodAppApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.backendmicroservicefoodapp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BackendMicroServiceFoodAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.backendmicroservicefoodapp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BackendMicroServiceFoodAppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
